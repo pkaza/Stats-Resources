92f472c52c646cf82f6bf2a9086363cc596bd71f
PK <kazamias.petros@gmail.com>
2021-05-31 20:00:19 +0300

    script1 update

--- r4sl_book/script1.R
+++ r4sl_book/script1.R
@@ -1,17 +1,28 @@
 #' ---
 #' title: "Modeling Basics in R"
-#' output:
-#' html_document:
-#' keep_md: true
+#' output: github_document
+#' #html_document:
+#' #keep_md: true
 #' ---
 
-#' Modeling Basics in R
-
+# Libraries 
 library(readr)
 library(here)
 
-here()
-#advertising = read_csv("data/Advertising.csv")
-advertising<-read_csv(here("data","Advertising.csv"))
-advertising
+# Load data
 
+Advertising<-read_csv(here("data","Advertising.csv"))
+Advertising<-Advertising[,-1]
+Advertising # Because the data was read using read_csv(), advertising is a tibble.
+
+# Visualization for regression
+
+plot(sales ~ TV, data = Advertising, col = "dodgerblue", pch = 20, cex = 1.5,main = "Sales vs Television Advertising")
+pairs(Advertising)
+
+library(caret)
+featurePlot(x = Advertising[ , c("TV", "radio", "newspaper")], y = Advertising$sales)
+
+# The lm() function
+#' The lm() function
+mod_1 = lm(sales ~ ., data = Advertising)
